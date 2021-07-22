911e973354f986408ebc80e2fc1324207f1e322e
areejobaid94 <areej.obaid@yahoo.com>
2021-07-21 19:12:22 -0700

    done

--- public/js/update_post.js
+++ public/js/update_post.js
@@ -27,8 +27,9 @@ window.onload = async function(){
   }
 
     let post = await feshPost();
+    console.log(post);
     if(post.error)console.log(post);
-    postData(post.post.text);
+    // postData(post.post.text);
 };
 
 function postData(data){
